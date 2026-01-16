4be0e6566f77f16774db1c9a898c509c0d545e49
Vicente Matallana <vistrent834@gmail.com>
2026-01-16 12:53:54 -0500

    chore: Modificaciones al enum ReconciliationStatus

--- src/main/java/dev/cluuny/frc/domain/model/ReconciliationStatus.java
+++ src/main/java/dev/cluuny/frc/domain/model/ReconciliationStatus.java
@@ -2,14 +2,11 @@ package dev.cluuny.frc.domain.model;
 
 public enum ReconciliationStatus {
     MATCHED,
-    DISCREPANCY_AMOUNT,
     MISSING_IN_BANK,
     MISSING_IN_INTERNAL,
     RECONCILED_EXACT,
     RECONCILED_TIME_TOLERANCE,
     POTENTIAL_MATCH,
-    UNMATCHED,
-    DUPLICATE,
     AMOUNT_MISMATCH,
     DATE_MISMATCH,
     AMBIGUOUS_MATCH
